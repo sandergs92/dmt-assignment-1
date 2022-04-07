9d06fe28f233b40329b7d726aea4f651c2cd9487
Sander <sandergs@protonmail.com>
2022-04-07 11:28:01 +0200

    Data to 2dim array, still needs cleaning up

--- assignment_1/helper.py
+++ assignment_1/helper.py
@@ -1,1 +1,6 @@
-# Functions will be defined here
+import numpy as np
+
+def get_data(filepath, datatype=('U64', 'U64', 'U64', bool, 'U64', 'U64', 'U64', 'U64', 'U64', np.int64, 'U64', np.int64, np.int64, np.float64, 'U64', 'U64', 'U64')):
+    with open(filepath) as file_name:
+        array = np.genfromtxt(file_name, dtype=datatype, delimiter=";", invalid_raise=False, skip_header=True)
+    return array
