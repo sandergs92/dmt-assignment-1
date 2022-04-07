269dff843513acc643468e1837087e79530ad77d
Sander <sandergs@protonmail.com>
2022-04-07 11:45:02 +0200

    small bugfix because dtype bool didn't work, gave only false

--- assignment_1/helper.py
+++ assignment_1/helper.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def get_data(filepath, datatype=('U64', 'U64', 'U64', bool, 'U64', 'U64', 'U64', 'U64', 'U64', np.int64, 'U64', np.int64, np.int64, np.float64, 'U64', 'U64', 'U64')):
+def get_data(filepath, datatype=('U64', 'U64', 'U64', np.int64, 'U64', 'U64', 'U64', 'U64', 'U64', np.int64, 'U64', np.int64, np.int64, np.float64, 'U64', 'U64', 'U64')):
     with open(filepath) as file_name:
         array = np.genfromtxt(file_name, dtype=datatype, delimiter=";", invalid_raise=False, skip_header=True)
     return array
